eee3384734d05bb8defbee9e1635bb6496ae8381
imran5644 <imi56444@gmail.com>
2020-12-07 23:49:26 +0530

    implements empty line error and end error

--- spec/checkers_spec.rb
+++ spec/checkers_spec.rb
@@ -16,4 +16,18 @@ describe CheckerError do
       expect(checker.errors[0]).to eql('line:2:31: Error: whitespace detected')
     end
   end
+
+  describe '#check_empty_line_errors' do
+    it 'it return empty line error on line 11' do
+      checker.check_empty_line_errors
+      expect(checker.errors[0]).to eql('line: 11 Extra Empty line detected at block body end')
+    end
+  end
+
+  describe '#end_error' do
+    it 'will return end error ' do
+      checker.end_error
+      expect(checker.errors[0]).to eql("Lint/Syntax: Missing 'end'")
+    end
+  end
 end
